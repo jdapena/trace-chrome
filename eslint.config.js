844b6c93e83d0a907463000bc6e5d4ff8da3020f
Jose Dapena Paz <jdapena@igalia.com>
2024-12-19 11:44:21 +0100

    Add eslint dependencies with google configuration

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+const globals = require('globals');
+const googleConfig = require('eslint-config-google');
+const js = require('@eslint/js');
+
+module.exports = [
+    js.configs.recommended,
+    googleConfig,
+    {
+        ignores: ["node_modules/"],
+        files: ["src/**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.mocha,
+            },
+            ecmaVersion: 9,
+            sourceType: "commonjs",
+        },
+    }
+];
