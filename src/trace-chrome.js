69cc46d3f8ab80d3187fc21d78e5e70e8e88728e
Jose Dapena Paz <jdapena@igalia.com>
2024-12-19 11:50:30 +0100

    First round of eslint

--- src/trace-chrome.js
+++ src/trace-chrome.js
@@ -10,99 +10,107 @@ program
     .option('-H, --host <host>', 'Remote debugging protocol host', 'localhost')
     .option('-p, --port <port>', 'Remote debugging protocool port', '9876')
     .option('-s, --showcategories', 'Show categories')
-    .option('-O, --output <path>', 'Output file', "")
-    .option('-c, --categories <categories>', 'Set categories', "")
-    .option('-e, --excludecategories <categories>', 'Exclude categories', "")
+    .option('-O, --output <path>', 'Output file', '')
+    .option('-c, --categories <categories>', 'Set categories', '')
+    .option('-e, --excludecategories <categories>', 'Exclude categories', '')
     .option('--systrace', 'Enable systrace')
-    .option('--memory_dump_mode <mode>', 'Memory dump mode', "")
+    .option('--memory_dump_mode <mode>', 'Memory dump mode', '')
     .option('--memory_dump_interval <interval_in_ms>',
-            'Memory dump interval in ms', 2000)
+        'Memory dump interval in ms', 2000)
     .option('--dump_memory_at_stop')
     .parse(process.argv);
 
-const cli_options = program.opts();
+const cliOptions = program.opts();
 
-const cdp_options = {
-  'host' : cli_options.host,
-  'port' : cli_options.port,
+const cdpOptions = {
+  'host': cliOptions.host,
+  'port': cliOptions.port,
 };
 
 function show_categories() {
-  CDP(cdp_options, (client) => {
+  CDP(cdpOptions, (client) => {
     const {Tracing} = client;
     Tracing.getCategories((message, result) => {
-      result.categories.forEach(category => { console.log(category); });
+      result.categories.forEach((category) => {
+        console.log(category);
+      });
       client.close();
     });
-  }).on('error', (err) => { console.error(err); });
+  }).on('error', (err) => {
+    console.error(err);
+  });
 }
 
 function trace_configuration_from_cli_options(opts) {
-  let result = {
-    dump_memory_mode : opts.memory_dump_mode,
-    dump_memory_at_stop : opts.dump_memory_at_stop,
-    dump_memory_interval : opts.memory_dump_interval,
-    output_file : opts.output,
-    trace_params : {traceConfig : {}, streamFormat : "json"}
+  const result = {
+    dump_memory_mode: opts.memory_dump_mode,
+    dump_memory_at_stop: opts.dump_memory_at_stop,
+    dump_memory_interval: opts.memory_dump_interval,
+    output_file: opts.output,
+    trace_params: {traceConfig: {}, streamFormat: 'json'},
   };
 
   if (opts.categories) {
-    console.error("Categories: " + opts.categories);
-    result.trace_params.traceConfig["includedCategories"] =
-        opts.categories.split(",");
+    console.error('Categories: ' + opts.categories);
+    result.trace_params.traceConfig['includedCategories'] =
+      opts.categories.split(',');
   }
   if (opts.excludecategories) {
-    console.error("Excluded categories: " + opts.excludecategories);
-    result.trace_params.traceConfig["excludedCategories"] =
-        opts.excludecategories.split(",");
+    console.error('Excluded categories: ' + opts.excludecategories);
+    result.trace_params.traceConfig['excludedCategories'] =
+      opts.excludecategories.split(',');
   }
   if (opts.systrace) {
-    console.error("Systrace enabled");
-    result.trace_params.traceConfig["enableSystrace"] = true;
+    console.error('Systrace enabled');
+    result.trace_params.traceConfig['enableSystrace'] = true;
   }
   if (opts.memory_dump_mode) {
-    console.error("Memory dump enabled. Mode " + opts.memory_dump_mode +
-                  ". Interval " + opts.memory_dump_interval + "ms");
-    if (!result.trace_params.traceConfig["includedCategories"]) {
-      result.trace_params.traceConfig["includedCategories"] = [ "*" ];
+    console.error('Memory dump enabled. Mode ' + opts.memory_dump_mode +
+      '. Interval ' + opts.memory_dump_interval + 'ms');
+    if (!result.trace_params.traceConfig['includedCategories']) {
+      result.trace_params.traceConfig['includedCategories'] = ['*'];
     }
-    result.trace_params.traceConfig["includedCategories"].push(
-        "disabled-by-default-memory-infra");
+    result.trace_params.traceConfig['includedCategories'].push(
+        'disabled-by-default-memory-infra');
   }
   return result;
 }
 
 function dump_memory(tracing, trace_config) {
-  const dump_options = {levelOfDetail : trace_config.dump_memory_mode};
+  const dump_options = {levelOfDetail: trace_config.dump_memory_mode};
   tracing.requestMemoryDump(dump_options)
       .then(
           function(result) {
-            console.error(`Memory dump ${result.success ? "done" : "failed"}`);
+            console.error(`Memory dump ${result.success ? 'done' : 'failed'}`);
           },
-          function() { console.error("Memory dump failed"); });
+          function() {
+            console.error('Memory dump failed');
+          });
 }
 
 function start_memory_dump(tracing, trace_config) {
-  return setInterval(function() { dump_memory(tracing, trace_config); },
-                     trace_config.dump_memory_interval);
+  return setInterval(function() {
+    dump_memory(tracing, trace_config);
+  },
+  trace_config.dump_memory_interval);
 }
 
 async function capture_trace(trace_config) {
-  CDP(cdp_options, (client) => {
+  CDP(cdpOptions, (client) => {
     const {Tracing} = client;
-    const data = {"traceEvents" : []};
+    const data = {'traceEvents': []};
 
     let dump_interval_id;
 
-    client.on('Tracing.dataCollected', message => {
+    client.on('Tracing.dataCollected', (message) => {
       data.trace_events = data.traceEvents =
-          data.traceEvents.concat(message.value);
+        data.traceEvents.concat(message.value);
     });
 
-    client.on('Tracing.tracingComplete', message => {
+    client.on('Tracing.tracingComplete', (message) => {
       const stringified_data = JSON.stringify(data);
       if (trace_config.output_file != '') {
-        try  {
+        try {
           fs.writeFileSync(trace_config.output_file, stringified_data);
         } catch (err) {
           console.error(err);
@@ -110,8 +118,9 @@ async function capture_trace(trace_config) {
       } else {
         console.log(stringified_data);
       }
-      if (message.dataLossOcurred)
-        console.error("Some data has been lost");
+      if (message.dataLossOcurred) {
+        console.error('Some data has been lost');
+      }
       client.close();
     });
 
@@ -119,14 +128,14 @@ async function capture_trace(trace_config) {
       dump_interval_id = start_memory_dump(Tracing, trace_config);
     }
 
-    console.error("Traceconfig is " + JSON.stringify(trace_config));
+    console.error('Traceconfig is ' + JSON.stringify(trace_config));
     Tracing.start(trace_config.trace_params);
     process.on('SIGINT', function() {
       if (dump_interval_id) {
         clearInterval(dump_interval_id);
       }
-      if (cli_options.dump_memory_at_stop) {
-        console.error("Dumping memory at stop");
+      if (cliOptions.dump_memory_at_stop) {
+        console.error('Dumping memory at stop');
         dump_memory(Tracing, trace_config);
       }
       Tracing.end();
@@ -134,9 +143,9 @@ async function capture_trace(trace_config) {
   }).on('error', (err) => console.error(err));
 }
 
-if (cli_options.showcategories) {
+if (cliOptions.showcategories) {
   show_categories();
 } else {
-  const trace_config = trace_configuration_from_cli_options(cli_options);
+  const trace_config = trace_configuration_from_cli_options(cliOptions);
   capture_trace(trace_config);
 }
