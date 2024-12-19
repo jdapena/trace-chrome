6627d0defb3eceaf2cea01b735152fe228a389f3
Jose Dapena Paz <jdapena@igalia.com>
2024-12-19 16:46:48 +0100

    Created lib/trace-chrome.js
    Now CLI support is in bin/trace-chrome only, anything else on
    lib/trace-chrome.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,26 @@ const googleConfig = require('eslint-config-google');
 const js = require('@eslint/js');
 
 module.exports = [
-    js.configs.recommended,
-    googleConfig,
-    {
-        ignores: ["node_modules/"],
-        files: ["src/**/*.js"],
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.mocha,
-            },
-            ecmaVersion: 9,
-            sourceType: "commonjs",
-        },
-        rules: {
-            "require-jsdoc": "off"
-        }
-    }
+  js.configs.recommended,
+  googleConfig,
+  {
+    ignores: [
+      'node_modules/',
+    ],
+    files: [
+      'bin/trace-chrome',
+      'lib/*.js',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
+      ecmaVersion: 9,
+      sourceType: 'commonjs',
+    },
+    rules: {
+      'require-jsdoc': 'off',
+    },
+  },
 ];
